2ca280e68fe52650b090a14864c76622ca6b1083
Prince-Kd <34554163+Prince-Kd@users.noreply.github.com>
2020-02-07 12:15:44 +0000

    First commit

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test{
+
+public static void main( String[] args){
+	int[] arr = {5, 4, 3, 2, 1};
+	int sum = 0; 
+	
+	for(int i =0; i <5; i++){
+		sum+=arr[i];
+	}
+
+	System.out.println("The sum of the array is: "+sum);
+
+	}
+}
+
